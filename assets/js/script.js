9a8448232fdc41c752d9e28e83315177c298c7f7
Felipe-M-dev <fmartinez.redes@gmail.com>
2021-06-04 00:16:49 -0400

    Rev1.3
    Firstview rev version final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/* Tooltip */
+
 $(function () {
 
     $("a").click(function (event) {
@@ -18,8 +20,10 @@ $(function () {
 
 });
 
+/* Sending mail message */
+
 $(document).ready(function () {
-    $("#btn_mail").click(function () {
+    $("#enviarCorreo").click(function () {
         var section_alert = $('#myalert');
 
         var nodo = `<div class="container-fluid alert alert-warning alert-dismissible fade show text-center" role="alert" style="width: 300px;">
@@ -33,8 +37,55 @@ $(document).ready(function () {
     });
 });
 
-let titulo = document.querySelector(".change__color")
+/* Add fav message */
 
-titulo.addEventListener("dblclick", () => {
-    titulo.classList.toggle("change_red")
+$(document).ready(function () {
+    $("#enviarFav").click(function () {
+        var section_alert = $('#myalert');
+
+        var nodo = `<div class="container-fluid bg-danger alert alert-warning alert-dismissible fade show text-center" role="alert" style="width: 300px;">
+      <strong class="text-white">Añadido a favoritos correctamente...</strong>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      </div>`
+        section_alert.append(nodo);
+
+    });
+});
+
+/* Change color text */
+
+let change = document.querySelector(".change__color")
+let change1 = document.querySelector(".change__color1")
+
+change.addEventListener("dblclick", () => {
+    change.classList.toggle("change__red")
 })
+
+change1.addEventListener("dblclick", () => {
+    change1.classList.toggle("change__red")
+})
+
+/* JQuery Toggle */
+
+$(document).ready(function () {
+    $("#card-toggle1").click(function () {
+        $("#p_text1").toggle();
+        $("#img_toggle1").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#card-toggle2").click(function () {
+        $("#p_text2").toggle();
+        $("#img_toggle2").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#card-toggle3").click(function () {
+        $("#p_text3").toggle();
+        $("#img_toggle3").toggle();
+    });
+});
